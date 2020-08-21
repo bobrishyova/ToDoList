f05b95b1411c5a2e2335f2df9b59c1c6e34d771a
Ангелина Бобришёва <angelina@MacBook-Pro-Angelina.local>
2020-08-21 11:33:25 +0300

    deleting and performing tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 
+const DoneTask = {
+  textDecoration: "line-through",
+}
+
 class ToDoApp extends React.Component {
   state = {
     items: [],
@@ -12,12 +16,16 @@ class ToDoApp extends React.Component {
   })
 
   addItem = () => {
+    if (this.state.text.length === 0) {
+      return null
+    }
     this.setState((state) => ({
       items: [
         ...state.items,
         {
           text: state.text,
           id: Date.now(),
+          done: false,
         }
       ],
       text: '',
@@ -25,18 +33,44 @@ class ToDoApp extends React.Component {
     )
   }
 
+  deleteTask = (id) => () => {
+    const filteredItems = this.state.items.filter((item) => item.id !== id)
+    this.setState({
+      items: filteredItems,
+    })
+  }
+
+  doneTask = (currentItem) => () => {
+    const newItems = this.state.items.map((item) => {
+      if (currentItem.id === item.id) {
+        return {
+          ...item,
+          done: !currentItem.done,
+        }
+      }
+      return item
+    })
+    this.setState({
+      items: newItems,
+    })
+  }
+
   render() {
     return (
       <div className="toDo">
-        <form>
+        <form className="toDoForm">
           <input type="text" value={this.state.text} placeholder="Task" onChange={this.saveInputText} />
           <button id="button" type="button" onClick={this.addItem}>Add</button>
         </form>
-        <ul>
-          {this.state.items.map((item) => (
-            <li key={item.id}>{item.text}</li>
-          ))}
-        </ul>
+        {this.state.items.map((item) => (
+          <div key={item.id} className="task">
+            <form className="checkbox">
+              <input type="checkbox" checked={item.done} onChange={this.doneTask(item)} />
+            </form>
+            <p className="text" style={item.done ? DoneTask : {}}>{item.text}</p>
+            <button className="button" onClick={this.deleteTask(item.id)}>&times;</button>
+          </div>
+        ))}
       </div>
     )
   }
